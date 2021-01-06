3d57fcd270f3d6a52734f77ea1be01714e421ade
Ahmed Mezar <a.mezar@lodexsolutions.local>
2021-01-06 15:51:00 +0200

    Updates

---
+++ precache-manifest.ee47a51182dd7e1c3926ff314228f99c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01787ff617645d6bde2b375460a1e3b6",
+    "url": "/News/index.html"
+  },
+  {
+    "revision": "2fa24a1d73cf382e9f25",
+    "url": "/News/static/css/2.57e18216.chunk.css"
+  },
+  {
+    "revision": "3c8b84302df2b5592086",
+    "url": "/News/static/css/main.45534e35.chunk.css"
+  },
+  {
+    "revision": "2fa24a1d73cf382e9f25",
+    "url": "/News/static/js/2.103a21c1.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/News/static/js/2.103a21c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8b84302df2b5592086",
+    "url": "/News/static/js/main.20f6499d.chunk.js"
+  },
+  {
+    "revision": "b6b7c6ebd6105be89b6e",
+    "url": "/News/static/js/runtime-main.fed28018.js"
+  },
+  {
+    "revision": "a14c20325bb25764f63bcfbc076e9201",
+    "url": "/News/static/media/Cairo-Bold.a14c2032.ttf"
+  },
+  {
+    "revision": "e793edb57aaf4d59ac9e943cbf091937",
+    "url": "/News/static/media/Cairo-Regular.e793edb5.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/News/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/News/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/News/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/News/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/News/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ae4eada39568dfca820bd60d8e8915a8",
+    "url": "/News/static/media/header.ae4eada3.jpg"
+  },
+  {
+    "revision": "2064c94546d54085b774d0556a1d4dfd",
+    "url": "/News/static/media/header_logo.2064c945.jpg"
+  },
+  {
+    "revision": "4bc3c2889c61c7b74bd83e74be3a9b40",
+    "url": "/News/static/media/menu.4bc3c288.svg"
+  },
+  {
+    "revision": "6da81e592f017ea5c2bcf84aab1e5971",
+    "url": "/News/static/media/x.6da81e59.svg"
+  }
+]);
